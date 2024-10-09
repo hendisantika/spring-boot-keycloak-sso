4eba9b79f71d2cf8c51a33c7663a8a245791b37d
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 11:44:45 +0700

    feat: Add StudentController 🫸🌀✏️📗🐧🐳⬆

---
+++ student-app/src/main/java/id/my/hendisantika/studentapp/controller/StudentController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.studentapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sso
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 11.44
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class StudentController {
+}
