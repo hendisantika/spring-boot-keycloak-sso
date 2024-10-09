bfe68896e4e5618c81d5593c5e133d6f27f6970b
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 11:39:56 +0700

    feat: Add student-app 🫸🌀✏️📗🐧🐳⬆

---
+++ student-app/src/test/java/id/my/hendisantika/studentapp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.studentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
