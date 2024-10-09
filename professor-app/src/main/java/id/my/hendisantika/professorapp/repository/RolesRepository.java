6570a0401acfc3409b29bba9fd9c0796815abe19
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 11:25:37 +0700

    Add RolesRepository 🫸🌀✏️📗🐧🐳⬆

---
+++ professor-app/src/main/java/id/my/hendisantika/professorapp/repository/RolesRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.professorapp.repository;
+
+import id.my.hendisantika.professorapp.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sso
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 11.25
+ * To change this template use File | Settings | File Templates.
+ */
+public interface RolesRepository extends JpaRepository<Roles, Long> {
+}
