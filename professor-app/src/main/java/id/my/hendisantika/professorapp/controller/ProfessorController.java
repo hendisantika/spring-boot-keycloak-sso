e09208c892c88cbd5508c9d9c38942f1f270ce63
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 11:37:33 +0700

    feat: ProfessorController 🫸🌀✏️📗🐧🐳⬆

---
+++ professor-app/src/main/java/id/my/hendisantika/professorapp/controller/ProfessorController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.professorapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sso
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/10/24
+ * Time: 11.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class ProfessorController {
+}
