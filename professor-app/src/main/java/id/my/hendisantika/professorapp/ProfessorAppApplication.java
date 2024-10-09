1baabf21db1c5d9364e45ec24b4b11e42a76affa
hendisantika <hendisantika@yahoo.co.id>
2024-10-09 11:18:08 +0700

    Create Professor App 🫸🌀✏️📗🐧🐳⬆

---
+++ professor-app/src/main/java/id/my/hendisantika/professorapp/ProfessorAppApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.professorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessorAppApplication.class, args);
+    }
+
+}
---
+++ professor-app/src/test/java/id/my/hendisantika/professorapp/ProfessorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.professorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
